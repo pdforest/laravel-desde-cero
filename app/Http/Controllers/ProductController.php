e3b6d97c7b8e9d0b07790b0914374672cf823bed
pdforest <pdforest@gmail.com>
2023-12-20 11:44:31 -0300

    Fin Seccion 8

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+    public function index() {
+
+        //$products = DB::table('products')->get();     // mediante Query Builder
+        //$products = Product::all();                     // mediante los modelos y el ORM Eloquent
+
+        //return $products;     // Larevel lo devuelve formateado en json 
+        //dd($products);        // usar debug() o dd() debug and die 
+
+        return view('products.index')->with([
+            //'products' => Product::all();
+            'products' => DB::table('products')->orderBy('id', 'desc')->get(),
+        ]);
+    }
+    
+    public function create() {
+        return view('products.create');
+    }
+
+    public function store() {
+
+        $rules = [
+            'title' => ['required', 'max:256'],
+            'description' => ['required', 'max:1000'],
+            'price' => ['required', 'min:1'],
+            'stock' => ['required', 'min:0'],
+            'status' => ['required', 'in:available,unavailable'],
+        ];
+
+        request()->validate($rules);
+
+        if( request()->status == 'available' && request()->stock == 0 ){
+            //session()->put('error', 'If available must have stock');
+            //session()->flash('error', 'If available must have stock');
+            
+            return redirect()
+                ->back()
+                ->withInput(request()->all())
+                ->withErrors('If available must have stock');
+        }
+        
+        /*$product = Product::create([
+            'title' => request('title'),
+            'description' => request('description'),
+            'price' => request('price'),
+            'stock' => request('stock'),
+            'status' => request('status'),
+        ]);*/
+        
+        $product = Product::create(request()->all());
+        //session()->flash('success', "The new product with id {$product->id} was created");
+
+        //return redirect()->back();
+        //return redirect()->action('MainController@index');
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The new product with id {$product->id} was created");
+            //->with(['success' => "The new product with id {$product->id} was created"]); // es lo mismo que withSuccess
+    }
+
+    public function show($product) {
+
+        //$product = DB::table('products')->where('id', $product)->first();
+        //$product = DB::table('products')->find($product);
+        $product = Product::findOrFail($product);
+
+        //return $product;
+        //dd($product);
+
+        return view('products.show')->with([
+            'product' => $product,
+        ]);
+    }
+
+    public function edit($product) {
+
+        return view('products.edit')->with([
+            'product' => Product::findOrFail($product),
+        ]);
+    }
+
+    public function update($product) {
+
+        $rules = [
+            'title' => ['required', 'max:256'],
+            'description' => ['required', 'max:1000'],
+            'price' => ['required', 'min:1'],
+            'stock' => ['required', 'min:0'],
+            'status' => ['required', 'in:available,unavailable'],
+        ];
+
+        request()->validate($rules);
+
+        //lo buscamos de nuevo para verificar que exista
+        $product = Product::findOrFail($product);
+
+        $product->update(request()->all());
+
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The product with id {$product->id} was edited");
+    }
+
+    public function destroy($product) {
+
+        $product = Product::findOrFail($product);
+
+        $product->delete();
+
+        return redirect()
+            ->route('products.index')
+            ->withSuccess("The product with id {$product->id} was deleted");
+    }
+
+}
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h1>Edit Product</h1>
+
+    <form method="POST" action="{{ route('products.update', ['product' => $product->id]) }}">
+        @csrf
+        @method('PUT')
+        <div class="form-row">
+            <label>Title</label>
+            <input class="form-control" type="text" name="title" required
+                value="{{ old('title') ?? $product->title }}">
+        </div>
+        <div class="form-row">
+            <label>Description</label>
+            <input class="form-control" type="text" name="description" required 
+                value="{{ old('description') ?? $product->description }}">
+        </div>
+        <div class="form-row">
+            <label>Price</label>
+            <input class="form-control" type="number" min="1.00" step="0.01" name="price" required 
+                value="{{ old('price') ?? $product->price }}">
+        </div>
+        <div class="form-row">
+            <label>Stock</label>
+            <input class="form-control" type="number" min="0" name="stock" required 
+                value="{{ old('stock') ?? $product->stock }}">
+        </div>
+        <div class="form-row">
+            <label>Status</label>
+            <select class="custom-select" name="status" required>
+                <option {{ old('status') == 'available' ? 'selected' : 
+                        ( $product->status == 'available' ? 'selected' : '' ) }}
+                        value="available">Available
+                </option>
+                <option {{ old('status') == 'unavailable' ? 'selected' : 
+                        ( $product->status == 'unavailable' ? 'selected' : '' ) }}
+                        value="unavailable">Unavailable
+                </option>
+            </select>
+        </div>
+        <div class="form-row">
+            <button type="submit" class="btn btn-primary btn-lg">Edit Product</button>
+        </div>
+
+    </form>
+
+@endsection
