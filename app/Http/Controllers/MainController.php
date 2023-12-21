81a65256052371b1282b5ff205834b2f07a7ac7c
pdforest <pdforest@gmail.com>
2023-12-21 12:35:14 -0300

    Fin Seccion 10

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+
+use App\Models\Product;
 
 class MainController extends Controller
 {
     public function index() {
-        return view('welcome');
+        return view('welcome')->with([
+            'products' => DB::table('products')->orderBy('id', 'desc')->get(),
+        ]);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+
+use App\Models\Product;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
@@ -15,14 +17,7 @@ class ProductController extends Controller
     
     public function index() {
 
-        //$products = DB::table('products')->get();     // mediante Query Builder
-        //$products = Product::all();                     // mediante los modelos y el ORM Eloquent
-
-        //return $products;     // Larevel lo devuelve formateado en json 
-        //dd($products);        // usar debug() o dd() debug and die 
-
         return view('products.index')->with([
-            //'products' => Product::all();
             'products' => DB::table('products')->orderBy('id', 'desc')->get(),
         ]);
     }
@@ -31,93 +26,39 @@ class ProductController extends Controller
         return view('products.create');
     }
 
-    public function store() {
+    public function store(ProductRequest $request) {
 
-        $rules = [
-            'title' => ['required', 'max:256'],
-            'description' => ['required', 'max:1000'],
-            'price' => ['required', 'min:1'],
-            'stock' => ['required', 'min:0'],
-            'status' => ['required', 'in:available,unavailable'],
-        ];
+        $product = Product::create($request->validated());
 
-        request()->validate($rules);
-
-        if( request()->status == 'available' && request()->stock == 0 ){
-            //session()->put('error', 'If available must have stock');
-            //session()->flash('error', 'If available must have stock');
-            
-            return redirect()
-                ->back()
-                ->withInput(request()->all())
-                ->withErrors('If available must have stock');
-        }
-        
-        /*$product = Product::create([
-            'title' => request('title'),
-            'description' => request('description'),
-            'price' => request('price'),
-            'stock' => request('stock'),
-            'status' => request('status'),
-        ]);*/
-        
-        $product = Product::create(request()->all());
-        //session()->flash('success', "The new product with id {$product->id} was created");
-
-        //return redirect()->back();
-        //return redirect()->action('MainController@index');
         return redirect()
             ->route('products.index')
             ->withSuccess("The new product with id {$product->id} was created");
-            //->with(['success' => "The new product with id {$product->id} was created"]); // es lo mismo que withSuccess
     }
 
-    public function show($product) {
-
-        //$product = DB::table('products')->where('id', $product)->first();
-        //$product = DB::table('products')->find($product);
-        $product = Product::findOrFail($product);
-
-        //return $product;
-        //dd($product);
+    public function show(Product $product) {
 
         return view('products.show')->with([
             'product' => $product,
         ]);
     }
 
-    public function edit($product) {
+    public function edit(Product $product) {
 
         return view('products.edit')->with([
-            'product' => Product::findOrFail($product),
+            'product' => $product,
         ]);
     }
 
-    public function update($product) {
+    public function update(ProductRequest $request, Product $product) {
 
-        $rules = [
-            'title' => ['required', 'max:256'],
-            'description' => ['required', 'max:1000'],
-            'price' => ['required', 'min:1'],
-            'stock' => ['required', 'min:0'],
-            'status' => ['required', 'in:available,unavailable'],
-        ];
-
-        request()->validate($rules);
-
-        //lo buscamos de nuevo para verificar que exista
-        $product = Product::findOrFail($product);
-
-        $product->update(request()->all());
+        $product->update($request->validated());
 
         return redirect()
             ->route('products.index')
             ->withSuccess("The product with id {$product->id} was edited");
     }
 
-    public function destroy($product) {
-
-        $product = Product::findOrFail($product);
+    public function destroy(Product $product) {
 
         $product->delete();
 
